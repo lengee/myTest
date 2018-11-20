0b98bf22f24550cde9a734928347d48ee26f1bc5
lengee <lengeeboy@163.com>
2018-11-20 09:04:33 +0800

    123

--- myTest/src/com/test/B.java
+++ myTest/src/com/test/B.java
@@ -1,11 +1,10 @@
 package com.test;
 
-public class A {
+public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1234");
-		System.out.println("asdf");
+
 	}
 
 }
